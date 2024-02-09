68d1d7342631643817b0bc09730114838b81dab7
Razphel <Brayan_TheDark@hotmail.com>
2024-02-09 11:57:19 +0100

    cositas

--- BBDD_Pruebas.sql
+++ BBDD_Pruebas.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS webreto;
 USE webreto;
 
 CREATE TABLE usuarios(
-id_usuarios INT AUTO_INCREMENT PRIMARY KEY,
+id_usuario INT AUTO_INCREMENT PRIMARY KEY,
 admin BOOLEAN DEFAULT FALSE, 
 nombre VARCHAR(50),
 email VARCHAR(50) NOT NULL,
@@ -15,7 +15,7 @@ telefono VARCHAR(15)
 );
  
 CREATE TABLE solicitudes(
-id_solicitudes INT AUTO_INCREMENT PRIMARY KEY,
+id_solicitud INT AUTO_INCREMENT PRIMARY KEY,
 fecha_solicitud DATE,
 descripcion VARCHAR(50),
 unidades VARCHAR(30),
@@ -24,11 +24,11 @@ observaciones TEXT,
 tramitado BOOLEAN DEFAULT FALSE,
 fk_usuario INT,
  
-CONSTRAINT fk_usuarios_solicitudes FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuarios)
+CONSTRAINT fk_usuarios_solicitudes FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuario)
 );
 
 CREATE TABLE proveedores(
-id_proveedores INT AUTO_INCREMENT PRIMARY KEY,
+id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(50),
 telefono VARCHAR(15),
 email VARCHAR(50),
@@ -37,69 +37,69 @@ observaciones TEXT
 );
 
 CREATE TABLE estados(
-id_estados INT AUTO_INCREMENT PRIMARY KEY,
+id_estado INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100)
 );
 
 CREATE TABLE pedidos(
-id_pedidos INT AUTO_INCREMENT PRIMARY KEY,
+id_pedido INT AUTO_INCREMENT PRIMARY KEY,
 fecha_pedido DATE,
 fk_proveedor INT,
 fk_estado INT,
 fk_usuario INT,
 observaciones TEXT,
 
-CONSTRAINT fk__pedidos_usuarios FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuarios),
-CONSTRAINT fk__pedidos_estados FOREIGN KEY (fk_estado) REFERENCES estados(id_estados),
-CONSTRAINT fk__pedidos_proveedores FOREIGN KEY (fk_proveedor) REFERENCES proveedores(id_proveedores)
+CONSTRAINT fk__pedidos_usuarios FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuario),
+CONSTRAINT fk__pedidos_estados FOREIGN KEY (fk_estado) REFERENCES estados(id_estado),
+CONSTRAINT fk__pedidos_proveedores FOREIGN KEY (fk_proveedor) REFERENCES proveedores(id_proveedor)
 );
 
 CREATE TABLE linea_pedido(
-id_linea_pedidos INT AUTO_INCREMENT PRIMARY KEY,
+id_linea_pedido INT AUTO_INCREMENT PRIMARY KEY,
 fk_pedido INT,
 descripcion VARCHAR(100),
 cantidad FLOAT,
 unidades VARCHAR(30),
 observaciones TEXT,
 
-CONSTRAINT fk_pedidos_linea_pedido FOREIGN KEY (fk_pedido) REFERENCES pedidos(id_pedidos)
+CONSTRAINT fk_pedidos_linea_pedido FOREIGN KEY (fk_pedido) REFERENCES pedidos(id_pedido)
 );
 
 CREATE TABLE unidades(
-id_unidades INT AUTO_INCREMENT PRIMARY KEY,
+id_unidad INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 observar TEXT
 );
 
 CREATE TABLE categorias(
-id_categorias INT AUTO_INCREMENT PRIMARY KEY,
+id_categoria INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 imagenes VARCHAR(50),
 observaciones TEXT
 );
 
 CREATE TABLE residuos(
-id_residuos INT AUTO_INCREMENT PRIMARY KEY,
+id_residuo INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100), 
 observaciones TEXT
 );
 
 CREATE TABLE productos(
-id_productos INT AUTO_INCREMENT PRIMARY KEY,
+id_producto INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 fk_unidad INT,
 observaciones TEXT,
 
-CONSTRAINT fk_unidades_productos FOREIGN KEY (fk_unidad) REFERENCES unidades(id_unidades)
+CONSTRAINT fk_unidades_productos FOREIGN KEY (fk_unidad) REFERENCES unidades(id_unidad)
 );
 
 CREATE TABLE producto_categoria(
-id_producto_categoria INT AUTO_INCREMENT PRIMARY KEY,
+id_productos_categoria INT AUTO_INCREMENT PRIMARY KEY,
 fk_producto INT,
 fk_categoria INT,
 
-CONSTRAINT fk_producto_categoria_categorias FOREIGN KEY (fk_categoria) REFERENCES categorias(id_categorias),
-CONSTRAINT fk_producto_categoria_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_productos)
+CONSTRAINT fk_producto_categoria_categorias FOREIGN KEY (fk_categoria) REFERENCES categorias(id_categoria),
+CONSTRAINT fk_producto_categoria_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_producto)
 );
 
 CREATE TABLE producto_residuo(
@@ -107,12 +107,12 @@ id_producto_residuo INT AUTO_INCREMENT PRIMARY KEY,
 fk_producto INT,
 fk_residuo INT,
 
-CONSTRAINT fk_producto_residuo_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_productos),
-CONSTRAINT fk_producto_residuo_residuos FOREIGN KEY (fk_residuo) REFERENCES residuos(id_residuos)
+CONSTRAINT fk_producto_residuo_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_producto),
+CONSTRAINT fk_producto_residuo_residuos FOREIGN KEY (fk_residuo) REFERENCES residuos(id_residuo)
 );
 
 CREATE TABLE estadoresiduos(
-id_estadoresiduos INT AUTO_INCREMENT PRIMARY KEY,
+id_estadoresiduo INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 observaciones TEXT
 );
@@ -127,7 +127,7 @@ observaciones TEXT,
 fecha_creacion DATE,
 fecha_desechado DATE,
 
-CONSTRAINT fk_residuos_generados_estadoresiduos FOREIGN KEY (fk_estadoresiduo) REFERENCES estadoresiduos(id_estadoresiduos)
+CONSTRAINT fk_residuos_generados_estadoresiduos FOREIGN KEY (fk_estadoresiduo) REFERENCES estadoresiduos(id_estadoresiduo)
 );
 
 INSERT INTO estados (descripcion) VALUES ('En reparto'),('Entregado'),('En preparacion'),('Problemas con el envio');
@@ -135,7 +135,7 @@ INSERT INTO proveedores (descripcion,telefono,email,direccion,observaciones) VAL
 INSERT INTO proveedores (descripcion,telefono,email,direccion,observaciones) VALUES ('EL CORTE INGLES','222222222','elcorteingles@gmail.com','Calle prueba 2, 32','observaciones prueba');
 INSERT INTO proveedores (descripcion,telefono,email,direccion,observaciones) VALUES ('MERCADONA','333333333','mercadona@gmail.com','Calle prueba 3, 12','observaciones prueba');
 INSERT INTO usuarios (admin,nombre,email,password,activo,observaciones,telefono) VALUES (1,'ADMINISTRADOR1','admin1@gmail.com','1234',1,'observaciones pruebas','888888888');
-INSERT INTO usuarios (admin,nombre,email,password,activo,observaciones,telefono) VALUES (0,'USUARIO1','usuario1@gmail.com','1235',0,'observaciones prueba','99999999');
+INSERT INTO usuarios (admin,nombre,email,password,activo,observaciones,telefono) VALUES (0,'sagaz','usuario1@gmail.com','1235',1,'observaciones prueba','99999999');
 INSERT INTO solicitudes (fecha_solicitud,descripcion,unidades,cantidad,observaciones,tramitado,fk_usuario) VALUES ('2024/02/07','huevos','cajas',6,'observaciones prueba',1,2);
 INSERT INTO solicitudes (fecha_solicitud,descripcion,unidades,cantidad,observaciones,tramitado,fk_usuario) VALUES ('2024/02/07','carne','kilogramos',3.4,'observaciones prueba',0,2);
 INSERT INTO pedidos (fecha_pedido,fk_proveedor,fk_estado,fk_usuario,observaciones) VALUES ('2024/02/07',1,3,2,'observacion kaladin');
--- usuario/php/consultaUsuario.php
+++ usuario/php/consultaUsuario.php
@@ -7,13 +7,6 @@ header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 // Indicamos  al navegador que va a recibir contenido JSON
 header("Content-Type: application/json");
 
-
-if (isset($_REQUEST['categoriaEnviada'])) {
-    $categoriaRecibida = $_REQUEST['categoriaEnviada'];
-    $productos = BD::consultaProductos($categoriaRecibida);
-    echo json_encode($productos);
-}
-
 if (isset($_REQUEST['claveUsuario'])) {
     $id_usuario = $_REQUEST['claveUsuario'];
     $historial = BD::imprimirPedidos($id_usuario);
