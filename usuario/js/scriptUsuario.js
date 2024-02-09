04e6424ba6e353758f7fd813d946655b0024c15a
Razphel <Brayan_TheDark@hotmail.com>
2024-02-09 09:31:59 +0100

    Empezando con el inicio de usuario

--- usuario/js/scriptUsuario.js
+++ usuario/js/scriptUsuario.js
@@ -1,11 +1,31 @@
 window.addEventListener("load", principal);
 
 function principal() {
+    //Antes de cargar la pagina del usuario, se comprueba que no se haya accedido sin una sesion valida.
+    if (localStorage.getItem("usuario")) {
+        let usuarioActual = JSON.parse(localStorage.getItem("usuario"));
+        if (usuarioActual.nombre === "") {
+            window.location.replace("../../inicio.html");
+        }
+    }
+
+    //Boton para cerrar la sesion y redireccionar a la pagina de inicio.
     document.querySelector("#cerrarSesion").addEventListener("click", cerrarSesion);
     let sesionActual = localStorage.getItem("usuario");
-    console.log(sesionActual.admin);
 
-    
+    $.ajax({
+        //Ubicacion del archivo php que va a manejar los valores.
+        url: "./php/consultaUsuario.php",
+        //Metodo en que los va a recibir.
+        type: "GET",
+        dataType: "json",
+        //La funcion que se ejecuta segun el resultado.
+        success: mostrarCategorias,
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error en la solicitud AJAX: " + textStatus, errorThrown);
+        }
+    });
+
 }
 
 function cerrarSesion() {
@@ -14,5 +34,47 @@ function cerrarSesion() {
     setTimeout(function () {
         window.location.replace("../../inicio.html");
     }, 500);
+}
+
+function mostrarCategorias(respuesta) {
+    //Ahora que tengo todos los datos de la tabla categorias, hago los elementos para guardarla.
+    let salida = document.querySelector("#categorias");
+
+    respuesta.forEach(fila => {
+        let contenedor = document.createElement("div");
+        contenedor.addEventListener("click", mostrarProductos);
+        contenedor.setAttribute("class", "col-3");
+        contenedor.style.border = "2px black solid";
+
+        let aux = document.createElement("p");
+        aux.innerHTML = fila.descripcion;
+
+        let aux2 = document.createElement("img");
+        aux2.setAttribute("href", fila.imagen);
+        contenedor.appendChild(aux2);
+        contenedor.appendChild(aux);
+        salida.appendChild(contenedor);
+    });
+
+}
+
+function mostrarProductos(e) {
+    //Selecciono el elemento p dentro del cuadro categoria en el que se haga click y recibo su texto, esto sera la categoria a mostrar.
+    let categoria = this.querySelector("p").textContent;
+
+    let parametros = { categoriaEnviada: categoria };
+
+    $.ajax({
+        //Ubicacion del archivo php que va a manejar los valores.
+        url: "./php/consultaUsuario.php",
+        //Metodo en que los va a recibir.
+        type: "GET",
+        dataType: "json",
+        //La funcion que se ejecuta segun el resultado.
+        success: mostrarCategorias,
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error en la solicitud AJAX: " + textStatus, errorThrown);
+        }
+    });
 }
 
--- usuario/php/consultaUsuario.php
+++ usuario/php/consultaUsuario.php
@@ -1,3 +1,12 @@
 <?php
 include_once "../../php/BD.class.php";
+// Para que el navegador no haga cache (fecha de expiración menor a la actual)
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 
+// Indicamos  al navegador que va a recibir contenido JSON
+header("Content-Type: application/json");
+
+$categorias = BD::consultaCategorias();
+
+echo json_encode($categorias);
