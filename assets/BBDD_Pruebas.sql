5e38b5b57a5dac6b9b1a59a65f5b7e4b732f6337
Laura Pérez <lperezm02@educantabria.es>
2024-02-20 12:09:39 +0100

    pequeños arreglos

--- assets/BBDD_Pruebas.sql
+++ assets/BBDD_Pruebas.sql
@@ -1,11 +1,11 @@
 DROP DATABASE IF EXISTS webreto;
 CREATE DATABASE IF NOT EXISTS webreto;
-
+ 
 USE webreto;
-
+ 
 CREATE TABLE usuarios(
-id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-admin BOOLEAN DEFAULT FALSE, 
+id_usuarios INT AUTO_INCREMENT PRIMARY KEY,
+admin BOOLEAN DEFAULT FALSE,
 nombre VARCHAR(50),
 email VARCHAR(50) NOT NULL,
 password VARCHAR(20),
@@ -15,7 +15,7 @@ telefono VARCHAR(15)
 );
  
 CREATE TABLE solicitudes(
-id_solicitud INT AUTO_INCREMENT PRIMARY KEY,
+id_solicitudes INT AUTO_INCREMENT PRIMARY KEY,
 fecha_solicitud DATE,
 descripcion VARCHAR(50),
 unidades VARCHAR(30),
@@ -24,99 +24,99 @@ observaciones TEXT,
 tramitado BOOLEAN DEFAULT FALSE,
 fk_usuario INT,
  
-CONSTRAINT fk_usuarios_solicitudes FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuario)
+CONSTRAINT fk_usuarios_solicitudes FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuarios)
 );
-
+ 
 CREATE TABLE proveedores(
-id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+id_proveedores INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(50),
 telefono VARCHAR(15),
 email VARCHAR(50),
 direccion VARCHAR(100),
 observaciones TEXT
 );
-
+ 
 CREATE TABLE estados(
-id_estado INT AUTO_INCREMENT PRIMARY KEY,
+id_estados INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100)
 );
-
+ 
 CREATE TABLE pedidos(
-id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+id_pedidos INT AUTO_INCREMENT PRIMARY KEY,
 fecha_pedido DATE,
 fk_proveedor INT,
 fk_estado INT,
 fk_usuario INT,
 observaciones TEXT,
-
-CONSTRAINT fk__pedidos_usuarios FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuario),
-CONSTRAINT fk__pedidos_estados FOREIGN KEY (fk_estado) REFERENCES estados(id_estado),
-CONSTRAINT fk__pedidos_proveedores FOREIGN KEY (fk_proveedor) REFERENCES proveedores(id_proveedor)
+ 
+CONSTRAINT fk__pedidos_usuarios FOREIGN KEY (fk_usuario) REFERENCES usuarios(id_usuarios),
+CONSTRAINT fk__pedidos_estados FOREIGN KEY (fk_estado) REFERENCES estados(id_estados),
+CONSTRAINT fk__pedidos_proveedores FOREIGN KEY (fk_proveedor) REFERENCES proveedores(id_proveedores)
 );
-
+ 
 CREATE TABLE linea_pedido(
-id_linea_pedido INT AUTO_INCREMENT PRIMARY KEY,
+id_linea_pedidos INT AUTO_INCREMENT PRIMARY KEY,
 fk_pedido INT,
 descripcion VARCHAR(100),
 cantidad FLOAT,
 unidades VARCHAR(30),
 observaciones TEXT,
-
-CONSTRAINT fk_pedidos_linea_pedido FOREIGN KEY (fk_pedido) REFERENCES pedidos(id_pedido)
+ 
+CONSTRAINT fk_pedidos_linea_pedido FOREIGN KEY (fk_pedido) REFERENCES pedidos(id_pedidos)
 );
-
+ 
 CREATE TABLE unidades(
-id_unidad INT AUTO_INCREMENT PRIMARY KEY,
+id_unidades INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 observar TEXT
 );
-
+ 
 CREATE TABLE categorias(
-id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+id_categorias INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 imagenes VARCHAR(50),
 observaciones TEXT
 );
-
+ 
 CREATE TABLE residuos(
-id_residuo INT AUTO_INCREMENT PRIMARY KEY,
-descripcion VARCHAR(100), 
+id_residuos INT AUTO_INCREMENT PRIMARY KEY,
+descripcion VARCHAR(100),
 observaciones TEXT
 );
-
+ 
 CREATE TABLE productos(
-id_producto INT AUTO_INCREMENT PRIMARY KEY,
+id_productos INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 fk_unidad INT,
 observaciones TEXT,
-
-CONSTRAINT fk_unidades_productos FOREIGN KEY (fk_unidad) REFERENCES unidades(id_unidad)
+ 
+CONSTRAINT fk_unidades_productos FOREIGN KEY (fk_unidad) REFERENCES unidades(id_unidades)
 );
-
+ 
 CREATE TABLE producto_categoria(
-id_productos_categoria INT AUTO_INCREMENT PRIMARY KEY,
+id_producto_categoria INT AUTO_INCREMENT PRIMARY KEY,
 fk_producto INT,
 fk_categoria INT,
-
-CONSTRAINT fk_producto_categoria_categorias FOREIGN KEY (fk_categoria) REFERENCES categorias(id_categoria),
-CONSTRAINT fk_producto_categoria_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_producto)
+ 
+CONSTRAINT fk_producto_categoria_categorias FOREIGN KEY (fk_categoria) REFERENCES categorias(id_categorias),
+CONSTRAINT fk_producto_categoria_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_productos)
 );
-
+ 
 CREATE TABLE producto_residuo(
 id_producto_residuo INT AUTO_INCREMENT PRIMARY KEY,
 fk_producto INT,
 fk_residuo INT,
-
-CONSTRAINT fk_producto_residuo_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_producto),
-CONSTRAINT fk_producto_residuo_residuos FOREIGN KEY (fk_residuo) REFERENCES residuos(id_residuo)
+ 
+CONSTRAINT fk_producto_residuo_productos FOREIGN KEY (fk_producto) REFERENCES productos(id_productos),
+CONSTRAINT fk_producto_residuo_residuos FOREIGN KEY (fk_residuo) REFERENCES residuos(id_residuos)
 );
-
+ 
 CREATE TABLE estadoresiduos(
-id_estadoresiduo INT AUTO_INCREMENT PRIMARY KEY,
+id_estadoresiduos INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
 observaciones TEXT
 );
-
+ 
 CREATE TABLE residuos_generados(
 id_residuos_generados INT AUTO_INCREMENT PRIMARY KEY,
 descripcion VARCHAR(100),
@@ -126,15 +126,16 @@ fk_estadoresiduo INT,
 observaciones TEXT,
 fecha_creacion DATE,
 fecha_desechado DATE,
-
-CONSTRAINT fk_residuos_generados_estadoresiduos FOREIGN KEY (fk_estadoresiduo) REFERENCES estadoresiduos(id_estadoresiduo)
+ 
+CONSTRAINT fk_residuos_generados_estadoresiduos FOREIGN KEY (fk_estadoresiduo) REFERENCES estadoresiduos(id_estadoresiduos)
 );
-
+ 
 INSERT INTO estados (descripcion) VALUES ('En reparto'),('Entregado'),('En preparacion'),('Problemas con el envio');
 INSERT INTO proveedores (descripcion,telefono,email,direccion,observaciones) VALUES ('CARREFOUR','111111111','carrefour@gmail.com','Calle prueba 1, 22','observaciones pruebas');
 INSERT INTO proveedores (descripcion,telefono,email,direccion,observaciones) VALUES ('EL CORTE INGLES','222222222','elcorteingles@gmail.com','Calle prueba 2, 32','observaciones prueba');
 INSERT INTO proveedores (descripcion,telefono,email,direccion,observaciones) VALUES ('MERCADONA','333333333','mercadona@gmail.com','Calle prueba 3, 12','observaciones prueba');
 INSERT INTO usuarios (admin,nombre,email,password,activo,observaciones,telefono) VALUES (1,'ADMINISTRADOR1','admin1@gmail.com','1234',1,'observaciones pruebas','888888888');
+INSERT INTO usuarios (admin,nombre,email,password,activo,observaciones,telefono) VALUES (0,'USUARIO1','usuario1@gmail.com','1235',1,'observaciones prueba','99999999');
 INSERT INTO usuarios (admin,nombre,email,password,activo,observaciones,telefono) VALUES (0,'sagaz','usuario1@gmail.com','1235',1,'observaciones prueba','99999999');
 INSERT INTO solicitudes (fecha_solicitud,descripcion,unidades,cantidad,observaciones,tramitado,fk_usuario) VALUES ('2024/02/07','huevos','cajas',6,'observaciones prueba',1,2);
 INSERT INTO solicitudes (fecha_solicitud,descripcion,unidades,cantidad,observaciones,tramitado,fk_usuario) VALUES ('2024/02/07','carne','kilogramos',3.4,'observaciones prueba',0,2);
@@ -146,10 +147,12 @@ INSERT INTO linea_pedido (fk_pedido,descripcion,cantidad,unidades,observaciones)
 INSERT INTO unidades (descripcion,observar) VALUES ('cajas','observacion kaladin');
 INSERT INTO unidades (descripcion,observar) VALUES ('kilogramos','observacion dalinar');
 INSERT INTO unidades (descripcion,observar) VALUES ('sacos','observacion adolin');
-INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('carne','observacion kaladin','imagen1.jpg');
-INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('pasteleria','observacion dalinar','imagen2.jpg');
-INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('pescado','observacion adolin','imagen3.jpg');
-INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('panaderia','observacion sagaz','imagen4.jpg');
+INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('carne','observacion kaladin','carniceria.jpg');
+INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('pasteleria','observacion dalinar','pasteleria.jpg');
+INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('pescado','observacion adolin','pescaderia.jpg');
+INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('panaderia','observacion sagaz','panaderia.jpg');
+INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('fruteria','observacion eshonai','fruteria.jpg');
+INSERT INTO categorias (descripcion,observaciones,imagenes) VALUES ('Cubiertos, servilletas...','observacion szeth','utiles.jpg');
 INSERT INTO residuos (descripcion,observaciones) VALUES ('caja de papel','observacion kaladin');
 INSERT INTO residuos (descripcion,observaciones) VALUES ('bolsa de plastico','observacion dalinar');
 INSERT INTO residuos (descripcion,observaciones) VALUES ('botellas de cristal','observacion adolin');
@@ -158,3 +161,7 @@ INSERT INTO productos (descripcion,fk_unidad,observaciones) VALUES ('carne de te
 INSERT INTO productos (descripcion,fk_unidad,observaciones) VALUES ('huevos',1,'observacion dalinar');
 INSERT INTO productos (descripcion,fk_unidad,observaciones) VALUES ('patatas',2,'observacion adolin');
 INSERT INTO productos (descripcion,fk_unidad,observaciones) VALUES ('tomate frito',1,'observacion sagaz');
+INSERT INTO producto_categoria (fk_producto,fk_categoria) VALUES (1,1);
+INSERT INTO producto_categoria (fk_producto,fk_categoria) VALUES (2,4);
+INSERT INTO producto_categoria (fk_producto,fk_categoria) VALUES (3,2);
+INSERT INTO producto_categoria (fk_producto,fk_categoria) VALUES (4,5);
--- assets/php/BD.php
+++ assets/php/BD.php
@@ -1,196 +1,184 @@
 <?php
+    class BD {
+        //En este caso no haremos uso de un constructor, ya que definiremos de primeras los atributos y definiremos los metodos como estaticos
+        private static $servidor = "localhost";
+        private static $basedatos = "webreto";
+        private static $usuario = "dwes";
+        private static $password = "abc123.";
 
-class BD
-{
-    //En este caso no haremos uso de un constructor, ya que definiremos de primeras los atributos y definiremos los metodos como estaticos
-    private static $servidor = "localhost";
-    private static $basedatos = "webreto";
-    private static $usuario = "dwes";
-    private static $password = "abc123.";
-
-    public function __get($atributo)
-    {
-        if (property_exists($this, $atributo)) {
-            return $this->$atributo;
-        }
-    }
-    public function __set($atributo, $valor)
-    {
-        if (property_exists($this, $atributo)) {
-            $this->$atributo = $valor;
-        }
-    }
-
-    public static function conexionBD()
-    {
-        $resultado = false;
-        try {
-
-            $conexion = new PDO('mysql:host=' . self::$servidor . ';dbname=' . self::$basedatos, self::$usuario, self::$password);
-
-            //Configura el nivel de error
-            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $resultado = $conexion;
-        } catch (PDOException $p) {
-            echo "<p>Error al conectar " . $p->getMessage() . "</p>";
-            $resultado = true;
-            exit();
-        }
-        return $resultado;
-    }
-
-    public static function consultaUsuario()
-    {
-        $respuesta = false;
-        try {
-            $conexion = self::conexionBD();
-            $aux1 = $_REQUEST['nombreUsuario'];
-            $aux2 = $_REQUEST['contraseña'];
-
-            $sql = "SELECT id_usuario, nombre, password, admin, activo FROM usuarios WHERE nombre= '$aux1' and password= '$aux2'";
-
-            // $preparada = $conexion->prepare($sql);
-            // $preparada->bindParam(':nombre', $_REQUEST['nombre']);
-            // $preparada->bindParam(':password', $_REQUEST['password']);
-            // $preparada->execute();
-
-            $resultado = $conexion->query($sql);
-
-            $fila = $resultado->fetch();
-            if ($fila) {
-                $respuesta = $fila;
-            } else {
-                $respuesta = false;
+        public function __get($atributo) {
+            if (property_exists($this, $atributo)) {
+                return $this->$atributo;
             }
-        } catch (Exception $e) {
-            throw new Exception("ERROR: " + $e);
         }
-        return $respuesta;
-    }
-
-    public static function consultaProductos($categoria)
-    {
-        try {
-            $conexion = self::conexionBD();
-            $sql = "SELECT * FROM productos WHERE categoria= '$categoria'";
-
-            $resultado = $conexion->query($sql);
-            $filas = [];
-
-            while ($fila = $resultado->fetch()) {
-                $filas[] = $fila;
+        public function __set($atributo, $valor) {
+            if (property_exists($this, $atributo)) {
+                $this->$atributo = $valor;
             }
-        } catch (Exception $e) {
-            throw new Exception("ERROR: " + $e);
         }
-        return $filas;
-    }
 
-    public static function consultaCategorias()
-    {
-        try {
-            $conexion = self::conexionBD();
-            $sql = "SELECT * FROM categorias";
+        public static function conexionBD() {
+            $resultado = false;
+            try {
 
-            $resultado = $conexion->query($sql);
+                $conexion = new PDO('mysql:host=' . self::$servidor . ';dbname=' . self::$basedatos, self::$usuario, self::$password);
 
-            // Crear un array para almacenar todas las filas        
-            $filas = [];
-            // Recorrer los resultados y almacenar cada fila en el array        
-            while ($fila = $resultado->fetch()) {
-                $filas[] = $fila;
+                //Configura el nivel de error
+                $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $resultado = $conexion;
+            } catch (PDOException $p) {
+                echo "<p>Error al conectar " . $p->getMessage() . "</p>";
+                $resultado = true;
+                exit();
             }
-        } catch (Exception $e) {
-            throw new Exception("ERROR: " + $e);
+            return $resultado;
         }
-        //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
-        return $filas;
-    }
 
-    public static function imprimirPedidos($usuarioInicioSesion)
-    {
-        try {
-            $conexion = self::conexionBD();
-            $sql = "SELECT fecha_pedido,descripcion,cantidad,unidades,linea_pedido.observaciones FROM pedidos inner join linea_pedido where pedidos.fk_usuario = $usuarioInicioSesion";
+        public static function consultaUsuario() {
+            $respuesta = false;
+            try {
+                $conexion = self::conexionBD();
+                $aux1 = $_REQUEST['nombreUsuario'];
+                $aux2 = $_REQUEST['contraseña'];
 
-            $resultado = $conexion->query($sql);
+                $sql = "SELECT id_usuario, nombre, password, admin, activo FROM usuarios WHERE nombre= '$aux1' and password= '$aux2'";
 
-            // Crear un array para almacenar todas las filas        
-            $filas = [];
-            // Recorrer los resultados y almacenar cada fila en el array        
-            while ($fila = $resultado->fetch()) {
-                $filas[] = $fila;
+                // $preparada = $conexion->prepare($sql);
+                // $preparada->bindParam(':nombre', $_REQUEST['nombre']);
+                // $preparada->bindParam(':password', $_REQUEST['password']);
+                // $preparada->execute();
+
+                $resultado = $conexion->query($sql);
+
+                $fila = $resultado->fetch();
+                if ($fila) {
+                    $respuesta = $fila;
+                } else {
+                    $respuesta = false;
+                }
+            } catch (Exception $e) {
+                throw new Exception("ERROR: " + $e);
             }
-        } catch (Exception $e) {
-            throw new Exception("ERROR: " + $e);
+            return $respuesta;
         }
-        //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
-        return $filas;
-    }
 
-    public static function insertarCategoria()
-    {
-        $resultado = false;
-        try {
-            $conexion = self::conexionBD();
-            $sql = "SELECT descripcion,observaciones,imagenes FROM categorias";
+        public static function consultaProductos($categoria) {
+            try {
+                $conexion = self::conexionBD();
+                $sql = "SELECT * FROM productos WHERE categoria= '$categoria'";
 
-            $resultado = $conexion->query($sql);
+                $resultado = $conexion->query($sql);
+                $filas = [];
 
-            // Crear un array para almacenar todas las filas        
-            $filas = [];
-            // Recorrer los resultados y almacenar cada fila en el array        
-            while ($fila = $resultado->fetch()) {
-                $filas[] = $fila;
+                while ($fila = $resultado->fetch()) {
+                    $filas[] = $fila;
+                }
+            } catch (Exception $e) {
+                throw new Exception("ERROR: " + $e);
             }
-        } catch (Exception $e) {
-            throw new Exception("ERROR: " + $e);
+            return $filas;
         }
-        //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
-        return $filas;
-    }
 
-    public static function eliminarCategoria()
-    {
-        $respuesta = false;
-        try {
-            $conexion = self::conexionBD();
-            $sql = "SELECT descripcion,observaciones,imagenes FROM categorias";
+        public static function consultaCategorias() {
+            try {
+                $conexion = self::conexionBD();
+                $sql = "SELECT * FROM categorias";
 
-            $resultado = $conexion->query($sql);
+                $resultado = $conexion->query($sql);
 
-            // Crear un array para almacenar todas las filas        
-            $filas = [];
-            // Recorrer los resultados y almacenar cada fila en el array        
-            while ($fila = $resultado->fetch()) {
-                $filas[] = $fila;
+                // Crear un array para almacenar todas las filas        
+                $filas = [];
+                // Recorrer los resultados y almacenar cada fila en el array        
+                while ($fila = $resultado->fetch()) {
+                    $filas[] = $fila;
+                }
+            } catch (Exception $e) {
+                throw new Exception("ERROR: " + $e);
             }
-        } catch (Exception $e) {
-            throw new Exception("ERROR: " + $e);
+            //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
+            return $filas;
         }
-        //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
-        return $filas;
-    }
 
-    public static function modificarCategoria()
-    {
-        $respuesta = false;
-        try {
-            $conexion = self::conexionBD();
-            $sql = "SELECT descripcion,observaciones,imagenes FROM categorias";
+        public static function imprimirPedidos($usuarioInicioSesion) {
+            try {
+                $conexion = self::conexionBD();
+                $sql = "SELECT fecha_pedido,descripcion,cantidad,unidades,linea_pedido.observaciones FROM pedidos inner join linea_pedido where pedidos.fk_usuario = $usuarioInicioSesion";
 
-            $resultado = $conexion->query($sql);
+                $resultado = $conexion->query($sql);
 
-            // Crear un array para almacenar todas las filas        
-            $filas = [];
-            // Recorrer los resultados y almacenar cada fila en el array        
-            while ($fila = $resultado->fetch()) {
-                $filas[] = $fila;
+                // Crear un array para almacenar todas las filas        
+                $filas = [];
+                // Recorrer los resultados y almacenar cada fila en el array        
+                while ($fila = $resultado->fetch()) {
+                    $filas[] = $fila;
+                }
+            } catch (Exception $e) {
+                throw new Exception("ERROR: " + $e);
             }
-        } catch (Exception $e) {
-            throw new Exception("ERROR: " + $e);
+            //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
+            return $filas;
         }
-        //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
-        return $filas;
+
+        public static function insertarCategoria() {
+            $resultado = false;
+            try {
+                $conexion = self::conexionBD();
+                $sql = "SELECT descripcion,observaciones,imagenes FROM categorias";
+
+                $resultado = $conexion->query($sql);
+
+                // Crear un array para almacenar todas las filas        
+                $filas = [];
+                // Recorrer los resultados y almacenar cada fila en el array        
+                while ($fila = $resultado->fetch()) {
+                    $filas[] = $fila;
+                }
+            } catch (Exception $e) {
+                throw new Exception("ERROR: " + $e);
+            }
+            //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
+            return $filas;
+        }
+
+        public static function eliminarCategoria() {
+            $respuesta = false;
+            try {
+                $conexion = self::conexionBD();
+                $sql = "SELECT descripcion,observaciones,imagenes FROM categorias";
+
+                $resultado = $conexion->query($sql);
+
+                // Crear un array para almacenar todas las filas        
+                $filas = [];
+                // Recorrer los resultados y almacenar cada fila en el array        
+                while ($fila = $resultado->fetch()) {
+                    $filas[] = $fila;
+                }
+            } catch (Exception $e) {
+                throw new Exception("ERROR: " + $e);
+            }
+            //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
+            return $filas;
+        }
+
+        public static function modificarCategoria() {
+            $respuesta = false;
+            try {
+                $conexion = self::conexionBD();
+                $sql = "SELECT descripcion,observaciones,imagenes FROM categorias";
+
+                $resultado = $conexion->query($sql);
+
+                // Crear un array para almacenar todas las filas        
+                $filas = [];
+                // Recorrer los resultados y almacenar cada fila en el array        
+                while ($fila = $resultado->fetch()) {
+                    $filas[] = $fila;
+                }
+            } catch (Exception $e) {
+                throw new Exception("ERROR: " + $e);
+            }
+            //Esta consulta te devuelve un array de arrays con todos los datos de la tabla producto.
+            return $filas;
+        }
     }
-}
