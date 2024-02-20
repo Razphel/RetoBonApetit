99a937958d5fd89fd72e6f3743baacaf98ec7886
Laura Pérez <lperezm02@educantabria.es>
2024-02-20 17:34:29 +0100

    actualizacion ruta relativa consultaUsuario.php

--- src/usuario/php/consultaUsuario.php
+++ src/usuario/php/consultaUsuario.php
@@ -1,17 +1,17 @@
 <?php
-include_once "../../php/BD.php";
-// Para que el navegador no haga cache (fecha de expiración menor a la actual)
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+    include_once "../../../assets/php/BD.php";
+    // Para que el navegador no haga cache (fecha de expiración menor a la actual)
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 
-// Indicamos  al navegador que va a recibir contenido JSON
-header("Content-Type: application/json");
+    // Indicamos  al navegador que va a recibir contenido JSON
+    header("Content-Type: application/json");
 
-if (isset($_REQUEST['claveUsuario'])) {
-    $id_usuario = $_REQUEST['claveUsuario'];
-    $historial = BD::imprimirPedidos($id_usuario);
-    echo json_encode($historial);
-} else {
-    $categorias = BD::consultaCategorias();
-    echo json_encode($categorias);
-}
+    if (isset($_REQUEST['claveUsuario'])) {
+        $id_usuario = $_REQUEST['claveUsuario'];
+        $historial = BD::imprimirPedidos($id_usuario);
+        echo json_encode($historial);
+    } else {
+        $categorias = BD::consultaCategorias();
+        echo json_encode($categorias);
+    }
