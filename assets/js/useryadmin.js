60c0af67fccd0fa6968706f2506ac4b8ddb96fd5
Laura Pérez <lperezm02@educantabria.es>
2024-02-21 10:00:32 +0100

    actualización archivos usuario

--- assets/js/useryadmin.js
+++ assets/js/useryadmin.js
@@ -24,5 +24,107 @@
 window.addEventListener("load", principal);
 
 function principal() {
+    //Antes de cargar la pagina del usuario, se comprueba que no se haya accedido sin una sesion valida.
+    if (localStorage.getItem("usuario")) {
+        let usuarioActual = JSON.parse(localStorage.getItem("usuario"));
+        if (usuarioActual.nombre === "") {
+            window.location.replace("../../../sesion.html");
+        }
+    } else {
+        //Redirige al usuario a la página de sesion no existen datos en el localStorage.
+        window.location.replace("../../../sesion.html");
+    }
 
+    // Boton para cerrar la sesion y redireccionar a la pagina de inicio.
+    document.querySelector("#cerrarSesion").addEventListener("click", cerrarSesion);
+}
+
+function cerrarSesion() {
+    localStorage.removeItem("usuario");
+
+    setTimeout(function () {
+        window.location.replace("../../../sesion.html");
+    }, 500);
+}
+
+function crearElemento(etiqueta, contenido, atributos) {
+    let elementoNuevo = document.createElement(etiqueta);
+    if (contenido !== undefined) {
+        let contenidoNuevo = document.createTextNode(contenido);
+        elementoNuevo.appendChild(contenidoNuevo);
+    }
+    if (atributos !== undefined) {
+        for (let clave in atributos) {
+            elementoNuevo.setAttribute(clave, atributos[clave]);
+        }
+    }
+    return elementoNuevo;
+}
+
+function crearFormulario(campos, contenedor) {
+    let formulario = crearElemento('form');
+
+    campos.forEach(campo => {
+        let etiqueta = campo.etiqueta || 'input';
+        let atributos = campo.atributos || {};
+        let contenido = campo.contenido || '';
+
+        let input = crearElemento(etiqueta);
+
+        for (let clave in atributos) {
+            input.setAttribute(clave, atributos[clave]);
+        }
+
+        if (contenido !== '') {
+            input.value = contenido;
+        }
+        formulario.appendChild(input);
+    });
+
+    if (contenedor instanceof HTMLElement) {
+        contenedor.appendChild(formulario);
+    } else {
+        console.error('El contenedor especificado no es un elemento HTML válido.');
+    }
+}
+
+//Consulta general para recibir productos. La funcion devuelve un array de objetos literales con los datos de los productos.
+function consultarProductos() {
+    let parametros = {
+        pedirProductos: true
+    };
+
+    $.ajax({
+        //Ubicacion del archivo php que va a manejar los valores.
+        url: "./php/consultaUsuario.php",
+        //Metodo en que los va a recibir.
+        type: "GET",
+        data: parametros,
+        dataType: "json",
+        success: guardarProductos,
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error en la solicitud AJAX: " + textStatus, errorThrown);
+        }
+    });
+
+    function guardarProductos(listaProdutos) {
+        //Creo un array donde guardo todos los productos como objetos literales.
+        let todosProductos = [];
+
+        for (let i = 0; i < listaProdutos.length; i++) {
+            //Creo un objeto literal con los datos de cada producto.
+            let producto = {
+                id_categoria: listaProdutos[i].Id_categoria,
+                imagen_categoria: listaProdutos[i].Imagen_categoria,
+                nombre_producto: listaProdutos[i].nombre_producto,
+                nombre_categoria: listaProdutos[i].nombre_categoria,
+                nombre_unidades: listaProdutos[i].nombre_unidades,
+                nombre_observaciones: listaProdutos[i].nombre_observaciones
+            }
+            //Lo agrego al array de productos.
+            todosProductos.push(producto);
+        }
+
+        localStorage.setItem("todosProductos", JSON.stringify(todosProductos));
+    }
 }
--- src/usuario/js/scriptUsuario.js
+++ src/usuario/js/scriptUsuario.js
@@ -15,19 +15,6 @@
 window.addEventListener("load", principal);
 
 function principal() {
-    //Antes de cargar la pagina del usuario, se comprueba que no se haya accedido sin una sesion valida.
-    if (localStorage.getItem("usuario")) {
-        let usuarioActual = JSON.parse(localStorage.getItem("usuario"));
-        if (usuarioActual.nombre === "") {
-            window.location.replace("../../../sesion.html");
-        }
-    } else {
-        //Redirige al usuario a la página de sesion no existen datos en el localStorage.
-        window.location.replace("../../../sesion.html");
-    }
-
-    // Boton para cerrar la sesion y redireccionar a la pagina de inicio.
-    document.querySelector("#cerrarSesion").addEventListener("click", cerrarSesion);
     document.querySelector("#navProductos").addEventListener("click", navCategorias);
     document.querySelector("#navPedidos").addEventListener("click", navPedidos);
     document.querySelector("#navProveedores").addEventListener("click", navProveedores);
@@ -80,56 +67,6 @@ function principal() {
 
     //Guardo en el localStorage el listado con todos los productos.
     consultarProductos();
-}
-
-
-//Consulta general para recibir productos. La funcion devuelve un array de objetos literales con los datos de los productos.
-function consultarProductos() {
-    let parametros = {
-        pedirProductos: true
-    };
-
-    $.ajax({
-        //Ubicacion del archivo php que va a manejar los valores.
-        url: "./php/consultaUsuario.php",
-        //Metodo en que los va a recibir.
-        type: "GET",
-        data: parametros,
-        dataType: "json",
-        success: guardarProductos,
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error("Error en la solicitud AJAX: " + textStatus, errorThrown);
-        }
-    });
-
-    function guardarProductos(listaProdutos) {
-        //Creo un array donde guardo todos los productos como objetos literales.
-        let todosProductos = [];
-
-        for (let i = 0; i < listaProdutos.length; i++) {
-            //Creo un objeto literal con los datos de cada producto.
-            let producto = {
-                id_categoria: listaProdutos[i].Id_categoria,
-                imagen_categoria: listaProdutos[i].Imagen_categoria,
-                nombre_producto: listaProdutos[i].nombre_producto,
-                nombre_categoria: listaProdutos[i].nombre_categoria,
-                nombre_unidades: listaProdutos[i].nombre_unidades,
-                nombre_observaciones: listaProdutos[i].nombre_observaciones
-            }
-            //Lo agrego al array de productos.
-            todosProductos.push(producto);
-        }
-
-        localStorage.setItem("todosProductos", JSON.stringify(todosProductos));
-    }
-}
-
-function cerrarSesion() {
-    localStorage.removeItem("usuario");
-
-    setTimeout(function () {
-        window.location.replace("../../../sesion.html");
-    }, 500);
 }
 
 function mostrarProveedores(proveedores) {
@@ -366,10 +303,6 @@ function mostrarPedidos(respuesta) {
 
 }
 
-function inicioUsuario() {
-    
-}
-
 function navPedidos() {
     //Mostrar Historial.
     //Se almacena en esta variable la información recogida desde el main.
@@ -548,18 +481,4 @@ function filtroCategoria(id_categoriaRecibido) {
 
     //Agrego el elemento al contenedor principal.
     contenedor.appendChild(contenidoCategoria);
-}
-
-function crearElemento(etiqueta, contenido, atributos) {
-    let elementoNuevo = document.createElement(etiqueta);
-    if (contenido !== undefined) {
-        let contenidoNuevo = document.createTextNode(contenido);
-        elementoNuevo.appendChild(contenidoNuevo);
-    }
-    if (atributos !== undefined) {
-        for (let clave in atributos) {
-            elementoNuevo.setAttribute(clave, atributos[clave]);
-        }
-    }
-    return elementoNuevo;
 }
