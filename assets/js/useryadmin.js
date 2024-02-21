d33bf2c8791cf75126093ba8cb5aa8967e2ae244
Laura Pérez <lperezm02@educantabria.es>
2024-02-21 09:14:42 +0100

    creación js común en assets para user y admin

---
+++ assets/js/useryadmin.js
@@ -0,0 +1,28 @@
+/*
+    Funciones comunes para el usuario y el administrador:
+        - cerrarSesion()
+        - crearElemento()
+        - crearFormulario()
+
+        Las siguientes funciones son dinámicas pero se llaman desde 
+        cada script (user y admin) y se sobreescribe para adaptarlo 
+        a cada perfil
+
+        //! función dinámica para consultar cualquier dato de la BBDD
+        - consultarCategorias()
+        - consultarProductos()
+        - consultarPedidos()
+        - consultarProveedores()
+
+        //! función dinámica para mostrar cualquier dato de la BBDD
+        - mostrarCategorias()
+        - mostrarProductos()
+        - mostrarPedidos()
+        - mostrarProveedores()
+*/
+
+window.addEventListener("load", principal);
+
+function principal() {
+
+}
