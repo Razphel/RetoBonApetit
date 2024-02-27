6b54645c93765b12c3aebe9d1f684479e656275a
Laura Pérez <lperezm02@educantabria.es>
2024-02-27 09:43:46 +0100

    icono usuarios valor actualizado

--- assets/js/useryadmin.js
+++ assets/js/useryadmin.js
@@ -85,7 +85,7 @@ function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 13)];
     }
     return color;
 }
